5787056bd37528fa7219b9e6077b6eceb68a17bc
黑历 <heili@furdevs.cn>
2025-10-18 21:36:05 +0800

    update v0.0.1a3

--- index.py
+++ index.py
@@ -112,6 +112,8 @@ def get_price_info(text):
             data_block = match.group(1)
             if not match:
                 print(f'发现记录： ID:{item[1]}, 价格:-1')
+            if int(item[1]) == 100300:
+                continue
             # 提取所有+数字 [数值]中的数值（忽略currency）
             value_pattern = re.compile(r'\+\d+\s+\[([\d.]+)\]')  # 匹配+数字 [x.x]格式
             values = value_pattern.findall(data_block)
@@ -122,21 +124,18 @@ def get_price_info(text):
                 num_values = min(len(values), 30)
                 sum_values = sum(float(values[i]) for i in range(num_values))
                 average_value = sum_values / num_values
-            with open("id_table.conf", 'r', encoding="utf-8") as f:
-                id_table = f.readlines()
-                # 格式 key[空格]value
-                id_dict = {}
-                for line in id_table:
-                    parts = line.replace("\n", "").split(' ')
-                    if len(parts) == 2:
-                        id_dict[parts[0]] = parts[1]
-            name = id_dict[ids]
-            price = json.load(open("price.json", "r", encoding="utf-8"))
-            price[name] = round(average_value, 4)
-            with open("price.json", "w", encoding="utf-8") as f:
-                json.dump(price, f, indent=4, ensure_ascii=False)
-
-            print(f'更新物品价值： ID:{ids}, 名称:{name}, 价格:{round(average_value, 4)}')
+            with open("full_table.json", 'r', encoding="utf-8") as f:
+                full_table = json.load(f)
+                try:
+                    full_table[ids]['last_time'] = round(time.time())
+                    #full_table[ids]['from'] = "Local"
+                    full_table[ids]['from'] = "FurryHeiLi"
+                    full_table[ids]['price'] = round(average_value, 4)
+                except:
+                    pass
+            with open("full_table.json", 'w', encoding="utf-8") as f:
+                json.dump(full_table, f, indent=4, ensure_ascii=False)
+            print(f'更新物品价值： ID:{ids}, 名称:{full_table[ids]["name"]}, 价格:{round(average_value, 4)}')
     except:
         pass
 
@@ -176,13 +175,14 @@ def scanned_log(changed_text):
     return tmp
 
 def deal_change(changed_text):
-    global is_in_map, all_time_passed, drop_list, income, t, drop_list_all, income_all
+    global is_in_map, all_time_passed, drop_list, income, t, drop_list_all, income_all, total_time
     if "PageApplyBase@ _UpdateGameEnd: LastSceneName = World'/Game/Art/Maps/01SD/XZ_YuJinZhiXiBiNanSuo200/XZ_YuJinZhiXiBiNanSuo200.XZ_YuJinZhiXiBiNanSuo200' NextSceneName = World'/Game/Art/Maps" in changed_text:
         is_in_map = True
         drop_list = {}
         income = 0
     if "NextSceneName = World'/Game/Art/Maps/01SD/XZ_YuJinZhiXiBiNanSuo200/XZ_YuJinZhiXiBiNanSuo200.XZ_YuJinZhiXiBiNanSuo200'" in changed_text:
         is_in_map = False
+        total_time += time.time() - t
     texts = scanned_log(changed_text)
     for i in texts:
         data = log_to_json(i)
@@ -196,24 +196,15 @@ def deal_change(changed_text):
             if data[item]["item"].get("SpecialInfo", True):
                 data[item]["item"]["BaseId"] = data[item]["item"]["SpecialInfo"]["BaseId"]
                 data[item]["item"]["Num"] = data[item]["item"]["SpecialInfo"]["Num"]
-            item_id = data[item]["item"]["BaseId"]
-            with open("id_table.conf", 'r', encoding="utf-8") as f:
-                id_table = f.readlines()
-                for i in id_table:
-                    _id = i.split(" ")[0]
-                    name = i.split(" ")[1].replace("\n", "")
-                    if _id == str(item_id):
-                        item_id = name
-                        break
-            drop_list[item_id] = drop_list.get(item_id, 0) + data[item]["item"]["Num"]
-            drop_list_all[item_id] = drop_list_all.get(item_id, 0) + data[item]["item"]["Num"]
-            price = 0
-            with open("price.json", "r", encoding="utf-8") as f:
-                j = json.load(f)
-                if item_id in j.keys():
-                    income += j[item_id] * data[item]["item"]["Num"]
-                    income_all += j[item_id] * data[item]["item"]["Num"]
-                    price = j[item_id]
+            item_id = str(data[item]["item"]["BaseId"])
+            with open("full_table.json", 'r', encoding="utf-8") as f:
+                full = json.load(f)
+            item_name = full[item_id]["name"]
+            price = full[item_id]["price"]
+            drop_list[item_name] = drop_list.get(item_name, 0) + data[item]["item"]["Num"]
+            drop_list_all[item_name] = drop_list_all.get(item_name, 0) + data[item]["item"]["Num"]
+            income += price * data[item]["item"]["Num"]
+            income_all += price * data[item]["item"]["Num"]
             print("掉落:" + str(item_id))
             with open("drop.txt", 'a', encoding="utf-8") as f:
                 now = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
@@ -226,6 +217,7 @@ income = 0
 income_all = 0
 t = time.time()
 show_all = False
+total_time = 0
 def change_states(words: tkinter.StringVar):
     global show_all
     show_all = not show_all
@@ -235,48 +227,69 @@ def change_states(words: tkinter.StringVar):
         words.set("目前：总掉落 点击切换当前地图掉落")
 class MyThread(threading.Thread):
     history = ""
-
     def run(self):
         global all_time_passed, income, drop_list, t
         self.history = open(position_log, "r", encoding="utf-8")
         self.history.read()
         from tkinter import Widget, Label
         import time
-        label_time = Label(text="地图耗时: 0m0s", font=("Arial", 12, "bold"), anchor="w")
+        scrollbar = tkinter.Scrollbar(root)
+        label_time = Label(text="地图耗时: 0m0s", font=("宋体", 12, "bold"), anchor="w")
         label_time.pack()
-        label_drop = Label(text="该地图掉落统计: 暂无", font=("Arial", 12, "bold"), anchor="w")
-        label_drop_all = Label(text="掉落总计: 0 火", font=("Arial", 12, "bold"), anchor="w")
+        label_time_all = Label(text="总耗时: 0m0s", font=("宋体", 12, "bold"), anchor="w")
+        label_time_all.pack()
+        label_drop = Label(text="该地图掉落统计: 暂无", font=("宋体", 12, "bold"), anchor="center", height=20)
+        label_drop_all = Label(text="掉落总计: 0 火", font=("宋体", 12, "bold"), anchor="w")
         words = tkinter.StringVar()
         words.set("目前：当前地图掉落 点击切换总掉落")
-        button_change = Button(textvariable=words, font=("Arial", 12, "bold"), command=lambda: change_states(words))
+        button_change = Button(textvariable=words, font=("宋体", 12, "bold"), command=lambda: change_states(words))
         button_change.pack()
-        label_drop.pack()
         label_drop_all.pack()
+        label_drop.pack()
+        button_clear = Button(text="清除掉落物记录", font=("宋体", 12, "bold"), command=lambda: {drop_list_all.clear()})
+        button_clear.pack()
         while True:
             time.sleep(1)
             try:
-                if is_in_map:
-                    m = int((time.time() - t) // 60)
-                    s = int((time.time() - t) % 60)
-                    label_time.config(text=f"地图耗时: {m}m{s}s")
-                else:
-                    t = time.time()
                 things = self.history.read()
                 #print(things)
                 deal_change(things)
                 get_price_info(things)
+                if is_in_map:
+                    m = int((time.time() - t) // 60)
+                    s = int((time.time() - t) % 60)
+                    label_time.config(
+                        text=f"地图耗时: {m}m{s}s 效率： {round(income / ((time.time() - t) / 60), 3)} 火/分钟")
+                    tmp_total_time = total_time + (time.time() - t)
+                    m = int(tmp_total_time // 60)
+                    s = int(tmp_total_time % 60)
+                    label_time_all.config(
+                        text=f"总耗时: {m}m{s}s 效率： {round(income_all / (tmp_total_time / 60), 2)} 火/分钟")
+                else:
+                    t = time.time()
                 if show_all:
                     label_drop_tmp = ""
                     for i in drop_list_all.keys():
-                        label_drop_tmp += f"{i} x{drop_list_all[i]} \n"
-                    label_drop.config(text=f"总掉落统计: \n{label_drop_tmp}")
+                        label_drop_tmp += f"{i} x{drop_list_all[i]}\n"
+                    if len(drop_list) < 15 :
+                        label_drop.config(text=f"该地图掉落统计: \n{label_drop_tmp}")
+                    else:
+                        label_drop.config(wraplength=650)
+                        label_drop_tmp = label_drop_tmp.replace("\n", "|")
+                        label_drop.config(text=f"总掉落统计:\n{label_drop_tmp}", width=70)
                     label_drop_all.config(text=f"掉落总计: {round(income_all, 3)} 火")
                 else:
                     label_drop_tmp = ""
                     for i in drop_list.keys():
                         label_drop_tmp += f"{i} x{drop_list[i]} \n"
-                    label_drop.config(text=f"该地图掉落统计: \n{label_drop_tmp}")
+                    if len(drop_list) < 15:
+                        label_drop.config(text=f"该地图掉落统计: \n{label_drop_tmp}")
+                    else:
+                        label_drop.config(wraplength=650)
+                        label_drop_tmp = label_drop_tmp.replace("\n", "|")
+                        label_drop.config(text=f"该地图掉落统计: \n{label_drop_tmp}", width=70)
                     label_drop_all.config(text=f"该地图掉落总计: {round(income, 3)} 火")
+
             except Exception as e:
                 print("发生异常，但问题不大，除非一直显示这个")
                 print("------------异常内容------------------\n")
@@ -287,6 +300,7 @@ class MyThread(threading.Thread):
 root = tkinter.Tk()
 root.wm_attributes('-topmost', 1)
 # 背景透明
-root.wm_attributes('-alpha', 0.7)
+root.wm_attributes('-alpha', 0.6)
+root.title("FurTorch V0.0.1a3")
 MyThread().start()
 root.mainloop()
